2cf5760164e37e8223d42ce396aebbc2f3d44368
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-12 21:35:31 +0200

    Initialize Auth. function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,5 @@ router.route("/:id").get(userController.getUser);
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/test", authController.protect);
 module.exports = router;
