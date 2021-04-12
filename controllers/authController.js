b94fe2e8f961d74761d2daa552fe04f3450f9ad6
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-12 23:08:16 +0200

    Protect routes with authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
 const testErr = (statusCode, message, res) => {
   res.status(statusCode).json({
     status: "fail",
@@ -53,24 +55,33 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.protect = async (req, res) => {
+exports.protect = catchAsync(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-    console.log("asdasd");
   }
   console.log(req.headers.authorization.split(" ")[1]);
   if (!token) {
     return testErr(500, "HATA", res);
   }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) return testErr(500, "HATA", res);
+
+  req.user = currentUser;
+  next();
+
   res.status(200).json({
     status: "success",
     token,
   });
-};
+});
 
 exports.forgotPassword = (req, res) => {
   res.status(500).json({
